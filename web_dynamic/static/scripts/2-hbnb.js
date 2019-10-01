699231253566b8b04471f5abdd8fdaf40dcd234b
Madez <781@holbertonschool.com>
2019-10-01 01:28:04 +0000

    Change - color

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,11 +1,13 @@
 $(function () {
 $.ajax({
-    url: 'http://0.0.0.0:5001/api/v1/status/',
+    url: 'http://localhost:5001/api/v1/status/',
     type: 'GET',
     success: function(res) {
-	$('DIV#api_status').addClass('available')
+    console.log("Hello");
+	$('#api_status').addClass('available')
 	},
     error: function() {
-        $('DIV#api_status').removeClass('available')
+        $('#api_status').removeClass('available')
     }
+});
 });
